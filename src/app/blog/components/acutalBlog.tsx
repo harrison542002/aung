d39b16e769118e836807b7b854109ea81557e00b
Harris <aungthiha12345mdy@gmail.com>
2023-08-05 20:28:15 +0630

    reduce spacing in blog

--- src/app/blog/components/acutalBlog.tsx
+++ src/app/blog/components/acutalBlog.tsx
@@ -16,79 +16,71 @@ type Props = {
 const ActualBlog = (props: Props) => {
   const { data } = useTina(props);
   return (
-    <div className="">
-      <div className="bg-gray-50 p-10 text-gray-800 rounded-md shadow-md">
-        <h2 className="lg:text-6xl xl:text-8xl md:text-5xl text-3xl font-semibold">
-          {data.blog.title}
-        </h2>
-        <p className="text-xs text-gray-400 pt-3">
-          {formatDate(data.blog.date as string)}
-        </p>
-        <div className="py-3 text-sm">
-          <TinaMarkdown
-            content={data.blog.intro}
-            components={{
-              p: (props) => <p className="py-5 text-justify" {...props} />,
-            }}
-          />
-          {data.blog.paragraph?.map((p) => {
-            switch (p?.__typename) {
-              case "BlogParagraphParagraphWithImage": {
-                return (
-                  <>
-                    <div className="flex justify-center items-center">
-                      <div className="relative aspect-video my-10 shadow-md w-full max-w-xl h-auto border">
-                        <Image
-                          src={p.paragraphImage as string}
-                          fill
-                          alt="blogging"
-                          className="absolute"
-                        />
-                      </div>
+    <div className="bg-gray-50 lg:p-10 md:p-5 xl:p-12 p-4 text-gray-800 rounded-md shadow-md">
+      <h2 className="lg:text-6xl xl:text-8xl md:text-5xl text-3xl font-semibold">
+        {data.blog.title}
+      </h2>
+      <p className="text-xs text-gray-400 pt-3">
+        {formatDate(data.blog.date as string)}
+      </p>
+      <div className="py-3 text-sm">
+        <TinaMarkdown
+          content={data.blog.intro}
+          components={{
+            p: (props) => <p className="py-5 text-justify" {...props} />,
+          }}
+        />
+        {data.blog.paragraph?.map((p) => {
+          switch (p?.__typename) {
+            case "BlogParagraphParagraphWithImage": {
+              return (
+                <>
+                  <div className="flex justify-center items-center">
+                    <div className="relative aspect-video my-10 shadow-md w-full max-w-xl h-auto border">
+                      <Image
+                        src={p.paragraphImage as string}
+                        fill
+                        alt="blogging"
+                        className="absolute"
+                      />
                     </div>
-                    <TinaMarkdown
-                      content={p.paragraph}
-                      components={{
-                        p: (props) => <p className="text-justify" {...props} />,
-                        h3: (props) => (
-                          <h3
-                            className="pt-5 text-xl font-semibold"
-                            {...props}
-                          />
-                        ),
-                      }}
-                    />
-                  </>
-                );
-              }
-              case "BlogParagraphParagraphWithoutImage": {
-                return (
-                  <>
-                    <TinaMarkdown
-                      content={p.paragraph}
-                      components={{
-                        p: (props) => <p className="text-justify" {...props} />,
-                        h3: (props) => (
-                          <h3
-                            className="pt-5 text-xl font-semibold"
-                            {...props}
-                          />
-                        ),
-                      }}
-                    />
-                  </>
-                );
-              }
+                  </div>
+                  <TinaMarkdown
+                    content={p.paragraph}
+                    components={{
+                      p: (props) => <p className="text-justify" {...props} />,
+                      h3: (props) => (
+                        <h3 className="pt-5 text-xl font-semibold" {...props} />
+                      ),
+                    }}
+                  />
+                </>
+              );
             }
-          })}
-        </div>
-        <Link href={"/blog"}>
-          <div className="text-gray-600 pt-10 flex items-center gap-3 group lg:text-base md:text-base xl:text-base text-xs">
-            <AiOutlineLeft className="group-hover:-translate-x-2 transition-transform duration-500" />
-            <p>Back To Blog Lists</p>
-          </div>
-        </Link>
+            case "BlogParagraphParagraphWithoutImage": {
+              return (
+                <>
+                  <TinaMarkdown
+                    content={p.paragraph}
+                    components={{
+                      p: (props) => <p className="text-justify" {...props} />,
+                      h3: (props) => (
+                        <h3 className="pt-5 text-xl font-semibold" {...props} />
+                      ),
+                    }}
+                  />
+                </>
+              );
+            }
+          }
+        })}
       </div>
+      <Link href={"/blog"}>
+        <div className="text-gray-600 pt-10 flex items-center gap-3 group lg:text-base md:text-base xl:text-base text-xs">
+          <AiOutlineLeft className="group-hover:-translate-x-2 transition-transform duration-500" />
+          <p>Back To Blog Lists</p>
+        </div>
+      </Link>
     </div>
   );
 };
