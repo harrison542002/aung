585ea34d5738543993da4cead7f506a4bf371679
Harris <aungthiha12345mdy@gmail.com>
2023-11-03 00:09:37 +0630

    Add meta data for blog

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -2,8 +2,23 @@ import SpacingLayout from "@/components/SpacingLayout";
 import React from "react";
 import client from "../../../../tina/__generated__/client";
 import ActualBlog from "../components/acutalBlog";
+import { Metadata } from "next";
 
-const Page = async ({ params }: { params: { slug: string } }) => {
+type Props = { params: { slug: string } };
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const blog = await client.queries.blog({
+    relativePath: `${params.slug}.mdx`,
+  });
+
+  return {
+    title: blog.data.blog.title,
+    keywords: [blog.data.blog.title],
+    description: blog.data.blog.intro,
+  };
+}
+
+const Page = async ({ params }: Props) => {
   const blog = await client.queries.blog({
     relativePath: `${params.slug}.mdx`,
   });
